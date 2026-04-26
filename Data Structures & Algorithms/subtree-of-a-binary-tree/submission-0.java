6de1c53f5baed26a33e2a2a12b2fca0c738c5edd
Matei Chiriac <59644487+matei-coder@users.noreply.github.com>
2026-04-26 17:35:42 +0300

    Add: subtree-of-a-binary-tree - submission-0

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-0.java
@@ -0,0 +1,22 @@
+class Solution {  
+    boolean ans = false; // schimbat în false
+
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        dfs(root, subRoot);
+        return ans;
+    }
+
+    public void dfs(TreeNode root, TreeNode subRoot) {
+        if (root == null) return;
+        if (comparetoroot(root, subRoot)) ans = true; // setezi true doar la match
+        dfs(root.left, subRoot);
+        dfs(root.right, subRoot);
+    }
+
+    public boolean comparetoroot(TreeNode root, TreeNode subroot) { // returneaza boolean
+        if (root == null && subroot == null) return true;
+        if (root == null || subroot == null) return false;
+        if (root.val != subroot.val) return false;
+        return comparetoroot(root.left, subroot.left) && comparetoroot(root.right, subroot.right);
+    }
+}
