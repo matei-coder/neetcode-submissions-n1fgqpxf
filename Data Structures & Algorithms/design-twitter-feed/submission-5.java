e4ed20c0266bee1d7ef4b79a249a4f04770a28a2
Matei Chiriac <59644487+matei-coder@users.noreply.github.com>
2026-05-03 22:57:50 +0300

    Add: design-twitter-feed - submission-5

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-5.java
@@ -0,0 +1,63 @@
+class Twitter {
+
+
+
+    class Tweet{
+        int time;
+        int tweetID;
+
+        Tweet(int time , int tweetID){
+            this.time = time;
+            this.tweetID = tweetID;
+        }
+    }
+
+
+    Map<Integer , List<Tweet>> posts = new HashMap<>();
+    Map<Integer, Set<Integer>> followers = new HashMap<>();
+    int timestamp = 0;
+    public Twitter() {
+        
+    }
+    
+    public void postTweet(int userId, int tweetID) {
+        if(!posts.containsKey(userId)){
+            posts.put(userId , new ArrayList<>());
+           
+        }
+        posts.get(userId).add(new Tweet(timestamp++ , tweetID));
+    }
+    
+    public List<Integer> getNewsFeed(int userId) {
+        List<Integer> ans = new ArrayList<>();
+        PriorityQueue<Tweet> pq = new PriorityQueue<>((a,b)->b.time - a.time);
+
+        if (posts.containsKey(userId)) {
+        pq.addAll(posts.get(userId));
+        }
+        if(followers.containsKey(userId))
+        for(int follower : followers.get(userId)){
+            if(posts.containsKey(follower))
+                pq.addAll(posts.get(follower));
+        }
+
+         while (!pq.isEmpty() && ans.size() < 10) {
+        ans.add(pq.poll().tweetID);
+        }
+
+        return ans;
+    }
+    
+    public void follow(int followerId, int followeeId) {
+        if(followerId==followeeId)return;
+        if(!followers.containsKey(followerId)){
+            followers.put(followerId , new HashSet<>());
+        }
+        followers.get(followerId).add(followeeId);
+    }
+    
+    public void unfollow(int followerId, int followeeId) {
+        if(followerId==followeeId)return;
+        followers.get(followerId).remove((Integer)followeeId);
+    }
+}
