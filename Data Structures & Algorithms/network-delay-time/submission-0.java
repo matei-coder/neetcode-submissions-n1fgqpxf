166bce216560fe7977fab50843e1e4befd900171
Matei Chiriac <59644487+matei-coder@users.noreply.github.com>
2026-05-26 13:40:27 +0200

    Add: network-delay-time - submission-0

---
+++ Data Structures & Algorithms/network-delay-time/submission-0.java
@@ -0,0 +1,58 @@
+class Solution {
+    public int networkDelayTime(int[][] times, int n, int k) {
+        int inf = Integer.MAX_VALUE/4;
+        int[][] dist = new int[n][n];
+        for(int i=0;i<n;i++)
+        Arrays.fill(dist[i] , inf);
+        for(int i=0;i<times.length;i++){
+                dist[times[i][0]-1][times[i][1]-1] = times[i][2];
+        }
+        k--;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print(dist[i][j]+" ");
+            }
+            System.out.println("");
+        }
+         System.out.println("");
+
+
+        int[] dur = new int[n];
+        Arrays.fill(dur , inf);
+        dur[k] = 0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->{
+            return a[1] - b[1];
+        });
+        pq.add(new int[]{ k , 0});
+        boolean[] viz = new boolean[n];
+        //viz[k] = true;
+        while(!pq.isEmpty()){
+            int[] curr = pq.poll();
+            int node = curr[0];
+            int time = curr[1];
+            System.out.println(node +" "+ time);
+            dur[node] = Math.min(time , dur[node]);
+            if(viz[node]) continue;
+            
+
+
+            for(int i=0;i<n;i++){
+                if(dist[node][i]!=inf){
+                    pq.add(new int[]{i , time + dist[node][i]});
+                }
+            }
+            viz[node] = true;
+        }
+
+        for(int x: dur) System.out.print(x+" ");
+        
+        int rasp = 0;
+        for(int x: dur){
+            if(x==inf) return -1;
+            rasp = Math.max(rasp , x);
+        }
+
+
+        return rasp;
+    }
+}
