12f5b25826bde9b5cbcfad3ef2587eb171902040
Matei Chiriac <59644487+matei-coder@users.noreply.github.com>
2026-05-26 13:51:21 +0200

    Add: network-delay-time - submission-4

---
+++ Data Structures & Algorithms/network-delay-time/submission-4.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int networkDelayTime(int[][] times, int n, int k) {
+        int inf = Integer.MAX_VALUE/4;
+        List<int[]>[] adj = new ArrayList[n];
+        for (int i = 0; i < n; i++) adj[i] = new ArrayList<>();
+
+        
+        for(int i=0;i<times.length;i++){
+                int[] deadaugat = {times[i][1]-1 , times[i][2]};
+                adj[times[i][0]-1].add(deadaugat);
+        }
+        k--;
+
+        int[] dur = new int[n];
+        Arrays.fill(dur , inf);
+        dur[k] = 0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->{
+            return a[1] - b[1];
+        });
+        pq.add(new int[]{ k , 0});
+        boolean[] viz = new boolean[n];
+        
+        while(!pq.isEmpty()){
+            int[] curr = pq.poll();
+            int node = curr[0];
+            int time = curr[1];
+            
+            dur[node] = Math.min(time , dur[node]);
+            if(viz[node]) continue;
+            viz[node] = true;
+            for(int i=0;i<adj[node].size();i++){               
+                    pq.add(new int[]{adj[node].get(i)[0] , time + adj[node].get(i)[1]});
+            }
+            
+        }
+
+     
+        
+        int rasp = 0;
+        for(int x: dur){
+            if(x==inf) return -1;
+            rasp = Math.max(rasp , x);
+        }
+
+
+        return rasp;
+    }
+}
