7345591bd32d1c5133309c6b7c6fdadf48c95889
Matei Chiriac <59644487+matei-coder@users.noreply.github.com>
2026-05-28 19:59:36 +0200

    Add: cheapest-flight-path - submission-3

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-3.java
@@ -0,0 +1,47 @@
+class Solution {
+
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        List<List<int[]>> adj = new ArrayList<>();
+        for(int i=0;i<n*(k+2);i++) adj.add(new ArrayList<>());
+        for(int[] flight:flights){
+            for(int i=0;i<k+1;i++){
+                adj.get(i*n+flight[0]).add(new int[]{(i+1)*n+flight[1] , flight[2]});
+            }
+        }
+
+
+
+        //Djikstra
+        int[] dist = new int[n*(k+2)];
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->{
+            return dist[a]-dist[b];
+        });
+
+        Arrays.fill(dist , Integer.MAX_VALUE/4);
+        dist[src]=0;  
+        pq.add(src); 
+        boolean viz[] = new boolean[n*(k+2)];   
+
+        while(!pq.isEmpty()){
+            int curr = pq.poll();
+            if(curr%n == dst) return dist[curr];
+            if(viz[curr]==true) continue;
+            viz[curr] =true;
+
+            for(int[] edge :adj.get(curr)){
+                int next = edge[0];
+                int val = edge[1];
+               
+
+                dist[next] = Math.min(dist[next] , dist[curr]+val);
+                pq.add(next);
+            }
+        }
+        return -1;
+
+
+
+
+
+    }
+}
