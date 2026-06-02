3368111f146c9390925574d6b4c025acbec1354c
Matei Chiriac <59644487+matei-coder@users.noreply.github.com>
2026-06-02 09:43:25 +0200

    Add: perfect-squares - submission-0

---
+++ Data Structures & Algorithms/perfect-squares/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int numSquares(int n) {
+        int[] nums = new int[101];
+        for(int i=0;i<=100;i++){
+            nums[i]=(i+1)*(i+1);
+        }
+        int[] dp = new int[n+1];
+        Arrays.fill(dp , Integer.MAX_VALUE);
+        dp[0] = 0;
+        
+        for(int num : nums){
+            for(int i=0;i<=n;i++){
+                
+                if(i-num>=0) dp[i] =Math.min(dp[i] , dp[i-num]+1);
+            }
+        }
+
+        for(int x: dp) System.out.print(x+ " ");
+
+
+        return dp[n];
+    }
+}
