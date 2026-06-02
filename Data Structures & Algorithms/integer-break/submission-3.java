8bf7059fb0447f758d58cb3066891e4543ccc938
Matei Chiriac <59644487+matei-coder@users.noreply.github.com>
2026-06-02 10:10:39 +0200

    Add: integer-break - submission-3

---
+++ Data Structures & Algorithms/integer-break/submission-3.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int integerBreak(int n) {
+        int[] dp = new int[n+1];
+        dp[0] =1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            for(int j=i;j>=1;j--){
+                dp[i] = Math.max(dp[i] , Math.max(j, dp[j])*(i-j));
+            }
+        }
+
+
+        return dp[n];
+    }
+}
