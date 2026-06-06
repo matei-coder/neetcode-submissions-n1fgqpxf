761f3d8b8a6fdc5412eb38c04a783e652bf1ed3c
Matei Chiriac <59644487+matei-coder@users.noreply.github.com>
2026-06-06 19:13:36 +0300

    Add: jump-game - submission-1

---
+++ Data Structures & Algorithms/jump-game/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int maxJump = 0;
+        for(int i=0;i<n;i++){
+            maxJump = max(maxJump , i + nums[i]);
+            //std::cout<<maxJump<<" ";
+            if(maxJump>= n-1) return true;
+            if(i==maxJump) return false;
+        }
+
+        return true;
+    }
+};
