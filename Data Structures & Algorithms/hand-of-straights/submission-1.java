5de01982e5057cb13f8571285f93c9b8b6e9f523
Matei Chiriac <59644487+matei-coder@users.noreply.github.com>
2026-06-08 17:08:54 +0300

    Add: hand-of-straights - submission-1

---
+++ Data Structures & Algorithms/hand-of-straights/submission-1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        int[] freq = new int[1001];
+        for(int x : hand) freq[x]++;
+        if(hand.length % groupSize != 0 )return false;
+        int groups = hand.length/groupSize;
+
+
+        Arrays.sort(hand);
+        for(int i=0;i<freq.length-groupSize;i++){
+            while(freq[i] >0 ){
+                for(int j = i ; j< i+groupSize;j++){
+
+                    if(freq[j] == 0) return false;
+                    freq[j]--;
+                    //ystem.out.print(j+ " ");
+                }
+
+                //System.out.println("");
+            }
+        }
+
+
+        return true;
+    }
+}
