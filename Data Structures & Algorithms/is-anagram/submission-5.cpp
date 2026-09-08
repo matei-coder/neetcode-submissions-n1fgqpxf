9ee7737f2f1adb2d5d858b8be259e3673a9d8dcf
Matei Chiriac <59644487+matei-coder@users.noreply.github.com>
2026-09-08 14:30:17 +0300

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int freqS[26]{};
+        int freqT[26]{};
+        for(char c : s){
+            freqS[c-'a']++;
+        }
+        for(char c : t){
+            freqT[c-'a']++;
+        }
+        for(int i=0;i<26;i++){
+            //cout<<freqS[i]<<" "<<freqT[i]<<endl;
+            if(freqS[i]!=freqT[i]) 
+            return false;
+        }
+        return true;
+
+    }
+};
