1330e22ee52e5cadbc67c50b3fcf1b3b32191776
Matei Chiriac <59644487+matei-coder@users.noreply.github.com>
2026-09-08 14:18:28 +0300

    Add: concatenation-of-array - submission-2

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int> ans;
+        int n = nums.size();
+        ans.reserve(2 * n);
+        for(int j=0;j<2;j++)
+        for(int i=0;i<n;i++){
+            ans.push_back(nums[i]);
+        }
+
+
+
+        return ans;
+    }
+};
