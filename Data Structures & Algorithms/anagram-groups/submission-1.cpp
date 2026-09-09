bc8961da56e1aa86afbd81aa41bad20516493957
Matei Chiriac <59644487+matei-coder@users.noreply.github.com>
2026-09-09 12:48:39 +0300

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string , vector<string>> groups;
+        for(string &s : strs){
+            string t = s;
+            sort(t.begin() , t.end());
+            groups[t].push_back(s); 
+        }
+        vector<vector<string>> ans;
+        for(auto &[key , group] : groups){
+            ans.push_back(group);
+
+        }
+
+        return ans;
+    }
+};
